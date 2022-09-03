b9ca5f6a4ee87948e51ecccf0d3067a7f6a1569f
Cris Arco <cris.arco83@gmail.com>
2022-09-03 10:49:53 +0200

    Lab finished

--- src/components/ContactsList.vue
+++ src/components/ContactsList.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="buttons-container">
-    <button>Add Random Contact</button>
-    <button>Sort by popularity</button>
-    <button>Sort by name</button>
+    <button @click="getRandomElem">Add Random Contact</button>
+    <button @click="sortByPopularity">Sort by popularity</button>
+    <button @click="sortByName">Sort by name</button>
   </div>
   <table v-if="fiveContacts.length" class="contacts-table">
     <tr>
@@ -11,6 +11,7 @@
       <th>Popularity</th>
       <th>Won Oscar</th>
       <th>Won Emmy</th>
+      <th>Actions</th>
     </tr>
     <tr class="contact-list" v-for="contact in fiveContacts" :key="contact.id">
       <td>
@@ -20,16 +21,15 @@
         {{ contact.name }}
       </td>
       <td>
-        {{ contact.popularity }}
+        {{ contact.popularity ? contact.popularity.toFixed(2) : 0 }}
       </td>
-      <td v-if="contact.wonOscar">
-        Yes!!
+      <td>
+        <img v-if="contact.wonOscar" src="../assets/trofeo.png">
       </td>
-      <td v-else></td>
-      <td v-if="contact.wonEmmy">
-        Yes!!
+      <td>
+        <img v-if="contact.wonEmmy" src="../assets/trofeo.png">
       </td>
-      <td v-else></td>
+      <td><button @click="deleteContact(contact.id)">Delete</button></td>
     </tr>
   </table>
 </template>
@@ -49,7 +49,39 @@ export default {
     for (let i = 0; i < 5; i++) {
       this.fiveContacts.push(this.contactsData.shift())
     }
-  }
+  },
+  methods: {
+    getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min) + min); 
+    },
+    getRandomElem() {
+      if (this.contactsData.length) {
+        const myRandomPos = this.getRandomInt(0, this.contactsData.length - 1);
+        const myRandomElem = this.contactsData[myRandomPos];
+        const elemIsAvailableToShow = this.fiveContacts.findIndex(elem => elem === myRandomElem);
+
+        if (elemIsAvailableToShow === -1) {
+          this.fiveContacts.push(myRandomElem);
+          const elemToRemoveIndex = this.contactsData.findIndex(elem => elem === myRandomElem);
+          this.contactsData.splice(elemToRemoveIndex, 1);
+        }
+      }
+    },
+    sortByName() {
+      this.fiveContacts.sort((a,b) => a.name > b.name ? 1 : -1);
+    },
+    sortByPopularity() {
+      this.fiveContacts.sort((a,b) => b.popularity - a.popularity);
+    },
+    deleteContact(contactId) {
+      const contactIndexFound = this.fiveContacts.findIndex(elem => elem.id === contactId);
+      if (contactIndexFound !== -1) {
+        this.contactsData.push(this.fiveContacts.splice(contactIndexFound,1)[0]);
+      }
+    }
+   },
 }
 </script>
 
